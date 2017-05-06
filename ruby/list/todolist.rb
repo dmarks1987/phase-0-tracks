f0c8e69612811baa94bbe94a491aad279b0b7122
DANNYPC\Danny <dmarks1987@gmail.com>
2017-05-06 10:49:25 -0700

    Add create todolist class with initialize, get items, add items, delete items

--- ruby/list/todolist.rb
+++ ruby/list/todolist.rb
@@ -1,27 +1,35 @@
-module ItemManagement
+class TodoList
+
+	attr_accessor :initial_array
+
+	def initialize (initial_array)
+		@initial_array = initial_array
+	end
 
 	def get_items()
+
 	end
 
 	def add_item(initial_array, to_add)
-		initial_array << to_add
-		initial_array
+		@initial_array << to_add
+		@initial_array
 	end
 
 	def delete_item(intial_array, to_delete)
-		initial_array.delete(to_delete)
-		initial_array
-	end
-
-
-end
-
-
-class TodoList
-	include ItemManagement
-
-	initialize (initial_array)
-		initial_array
+		@initial_array.delete(to_delete)
+		@initial_array
 	end
 
 end
+
+list = TodoList.new(["butts", "asses"])
+
+p list
+
+list.add_item(list, "penis")
+
+p list
+
+list.delete_item(list, "penis")
+
+p list
