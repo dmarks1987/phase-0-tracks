f7573070f7a4550267ee45ed72518677d270c027
DANNYPC\Danny <dmarks1987@gmail.com>
2017-05-07 16:17:19 -0700

    Add completed TodoList class and todo_list_spec test file.

--- ruby/list/todolist.rb
+++ ruby/list/todolist.rb
@@ -1,35 +1,40 @@
 class TodoList
 
-	attr_accessor :initial_array
+	attr_accessor :initial_array, :current_array
 
 	def initialize (initial_array)
-		@initial_array = initial_array
+		@current_array = initial_array
+		@current_array
 	end
 
-	def get_items()
-
+	def get_items
+		@current_array
 	end
 
-	def add_item(initial_array, to_add)
-		@initial_array << to_add
-		@initial_array
+	def get_item(index)
+		@current_array[index]
 	end
 
-	def delete_item(intial_array, to_delete)
-		@initial_array.delete(to_delete)
-		@initial_array
+	def add_item(to_add)
+		@current_array << to_add
+		@current_array
+	end
+
+	def delete_item(to_delete)
+		@current_array.delete(to_delete)
+		@current_array
 	end
 
 end
 
-list = TodoList.new(["butts", "asses"])
+# list = TodoList.new(["butts", "asses"])
 
-p list
+# p list
 
-list.add_item(list, "penis")
+# list.add_item(list, "penis")
 
-p list
+# p list
 
-list.delete_item(list, "penis")
+# list.delete_item(list, "penis")
 
-p list
+# p list
