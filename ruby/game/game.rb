e08823cda36d97c68c2f08a82755af6236985dc4
DANNYPC\Danny <dmarks1987@gmail.com>
2017-05-08 00:03:46 -0700

    Add initialize, guessing, and win condition methods to game.rb and test to make sure user input is taken properly in game_spec.rb

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -2,32 +2,62 @@
 # Ask user to enter a word
 # Store word into an array of separated letters called word_array
 # Create variable guess_count equal to number of letters in the array
-# Initialize game class with the array of letters
+# Initialize game class with the word_array and the guess_count
+# Show player 2 the amount of letters in the word represented by underscores
+# Ask user to guess the word one letter at a time
+# IF the user guesses a letter that is in the word
+  # acknowledge their correctness with an updated underscore word with the letter revealed
+  # OTHERWISE
+    # tell them their guess was incorrect and to guess again
+    # add 1 to the guess count
 
-# Show 
+
 class Game
-  attr_reader :word_array, :is_over
-  attr_writer :guess_count
+  attr_accessor :users_word, :hidden_word
 
-  def initialize(word_array, guess_count)
-    @word_array = word_array
-    @guess_count = guess_count
+# Initialize the instance with a word that is inputed by the user. 
+  def initialize(users_word)
+    @users_word = users_word
+    @hidden_word = "_ " * users_word.length
   end
 
-  def store_initial_data
-    @word_array
+  def store_user_input
+    @users_word
   end
 
-  def check_guess(word_array, letter_guessed)
-    if @word_array.include? letter_guessed
+# CHECK LETTER GUESS OF USER
+# IF the user guesses a letter that is in the word
+  # put that letter into the proper place  in the hidden word
+  # acknowledge their correctness with an updated hidden word with the letter revealed
+  # OTHERWISE
+    # tell them their guess was incorrect and to guess again
+  def check_guess(letter_guessed)
+    letter_guessed.downcase
+    @users_word.split("").each_with_index do|user_letter, hidden_letter|
+      if letter_guessed = user_letter
+        @hidden_word[hidden_letter] = letter_guessed
+        p @hidden_word
+      else
+        puts "#{letter_guessed} is not in the word. Try again."
+      end
+    end
+  end
+
+  def win_or_lose
+    if @users_word == @hidden_word
+      puts "You just won. You're amazing."
+    else
+      puts "Wow, you're a freaking loser."
+    end
   end
 end
 
-puts "Player 1 please type in a word you wish Player 2 to guess."
 
-word_input = gets.chomp.downcase
-word_array = []
-word_array = word_input.split('')
-guess_count = word_array.length
 
-game = Game.new(word_array, guess_count)
+# # User interface
+# puts "Player 1 please enter a word."
+# word_input = gets.chomp.downcase
+
+
+# game = Game.new(word_input)
+
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,10 +1,10 @@
 require_relative 'game'
 
 describe Game do
-  let(:game) { Game.new(["a","b"], 2) }
+  let(:game) { Game.new("lakers") }
 
   it "stores the word array and guess count on initialization" do
-    expect(game.store_initial_data).to eq ["a", "b"]
+    expect(game.store_user_input).to eq "lakers"
   end
-  
+
 end
