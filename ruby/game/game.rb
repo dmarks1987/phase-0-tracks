5107b57a3c169ddfa839e1fce9de91c11759b76d
DANNYPC\Danny <dmarks1987@gmail.com>
2017-05-08 08:42:45 -0700

    Add updated game.rb. The game still doesn't run properly. Need to fix the game loop so that it returns guesses and stored guesses properly.

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,17 +1,3 @@
-# USER INTERFACE
-# Ask user to enter a word
-# Store word into an array of separated letters called word_array
-# Create variable guess_count equal to number of letters in the array
-# Initialize game class with the word_array and the guess_count
-# Show player 2 the amount of letters in the word represented by underscores
-# Ask user to guess the word one letter at a time
-# IF the user guesses a letter that is in the word
-  # acknowledge their correctness with an updated underscore word with the letter revealed
-  # OTHERWISE
-    # tell them their guess was incorrect and to guess again
-    # add 1 to the guess count
-
-
 class Game
   attr_accessor :users_word, :hidden_word, :guess_count
 
@@ -37,7 +23,7 @@ class Game
     # tell them their guess was incorrect and to guess again
   def check_guess(letter_guessed)
     letter_guessed.downcase
-    @users_word.split("").each_with_index do|user_letter, hidden_letter|
+    @users_word.split('').each_with_index do|user_letter, hidden_letter|
       if letter_guessed = user_letter
         @hidden_word[hidden_letter] = letter_guessed
         p @hidden_word
@@ -59,6 +45,11 @@ class Game
 end
 
 # User interface
+
+# Ask user to enter a word
+# Store word into an array of separated letters called word_array
+# Create variable guess_count equal to number of letters in users_word
+# Initialize game class with the users_word
 puts "Player 1 please enter a word."
 word_input = gets.chomp.downcase
 
@@ -66,8 +57,10 @@ game = Game.new(word_input)
 
 inputed_letters = []
 guess_count = word_input.length
-guess_increment = 0
 
+# Show player 2 the amount of letters in the word represented by underscores
+# Ask user to guess the word one letter at a time
+guess_increment = 0
 until guess_increment == guess_count
   puts "Guess a letter."
   next_letter = gets.chomp.downcase
