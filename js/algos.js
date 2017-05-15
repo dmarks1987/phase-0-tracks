22b6057fed4c96277f2c000b8ec0edee908f0336
DANNYPC\Danny <dmarks1987@gmail.com>
2017-05-14 17:24:23 -0700

    Add LongestPhrase function to algos.js

--- js/algos.js
+++ js/algos.js
@@ -1,11 +1,15 @@
-function LongestPhrase(array)
-{
+// -loop needs some where to begin comparing, so we start
+// 	the currentLength at 0.
+// -declare longest variable to store the length of the array
+// 	if the length is greater than the currentLength.
+// -the loop will always run for every element in the array 
+//  and execute the if statement at least once because the 
+//  currentLength is initialized to 0.
+function LongestPhrase(array){
 	var currentLength = 0;
 	var longest;
-	for(var i = 0; i < array.length; i++)
-	{
-		if(array[i].length > currentLength )
-		{
+	for(var i = 0; i < array.length; i++){
+		if(array[i].length > currentLength ){
 			currentLength  = array[i].length;
 			longest = array[i];
 		}
