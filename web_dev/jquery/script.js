29b2d98af187d624f36de754846a7bf9790233df
DANNYPC\Danny <dmarks1987@gmail.com>
2017-05-26 17:16:52 -0700

    Add buttons with jQuery event handlers for shapes created in CSS.

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,6 +1,14 @@
-
 $(document).ready(function() {
-    $("#danny").slideUp(1000).delay(1000).slideDown(1000);
-    $("#j").fadeToggle(1000).fadeToggle(1000);
-    $("#marks");
+
+    $("#btn1").on('click', function(){
+    	$("#danny").slideUp(1000).delay(1000).slideDown(1000)
+    });
+
+    $("#btn2").on('click', function(){
+			$("#j").fadeToggle(1000).fadeToggle(1000);
+    });
+    
+    $("#btn3").on('click', function(){
+    	$("#marks").toggle(1000).toggle(1000);
+    });
 });
