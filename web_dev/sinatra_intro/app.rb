4e898ce7bec0ca022e4c8947e96e8fb56463e704
DANNYPC\Danny <dmarks1987@gmail.com>
2017-05-29 08:24:27 -0700

    Add app.rb with contact and great_job routes.

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,16 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+get '/contact' do
+ 	"28331 Quiet Vista, Seattle, WA 98015"
+end
+
+get '/great_job/:name' do
+  name = params[:name]
+	if name
+		"Good job, #{name}"
+	else
+		"Good job!"
+	end
+end
